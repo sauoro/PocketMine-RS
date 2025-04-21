1f46903287e98fd96418d6074ab0c6bce4841146
sauoro <sqwxro@gmail.com>
2025-04-21 14:17:14 -0700

    Delete src/raklib directory

--- src/raklib/generic/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod reliability;
-pub mod session;
-// Potentially socket base traits/structs later
-
-pub use reliability::{ReceiveReliabilityLayer, SendReliabilityLayer, ReliableCacheEntry};
-pub use session::Session; // Placeholder
--- src/raklib/utils/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod address;
-
-// Re-export
-pub use address::InternetAddress;
